c4dc5734a4dce5fbb810e2d6141ab72cb0c48705
Mihael Zeme (puhi0210) <mz2252@student.uni-lj.si>
2023-10-26 22:02:16 +0200

    Modifikacija DN

--- Domace_naloge/DN3/Naloga.py
+++ Domace_naloge/DN3/Naloga.py
@@ -10,11 +10,15 @@ with open('C:/Users/Mihael Zeme/Documents/Sola/TelekomunikacijskiProtokoli/Vaje/
 
 zacetniSlovar = {x["name"]:x for x in zacetniData["persons"]}
 
+print(zacetniSlovar)
+print(zacetniData)
 
 for oseba in updateData["persons"]:
     if oseba["name"] in zacetniSlovar.keys():
         zacetniSlovar[oseba["name"]].update(oseba)
 
+print(zacetniSlovar)
+print(zacetniData)
 
-with open('C:/Users/Mihael Zeme/Documents/Sola/TelekomunikacijskiProtokoli/Vaje/VAJE/Domace_naloge/DN3/DATA/noviData.json', 'w') as f:
+with open('C:/Users/Mihael Zeme/Documents/Sola/TelekomunikacijskiProtokoli/Vaje/VAJE/Domace_naloge/DN3/DATA/zacetniData.json', 'w') as f:
     json.dump(zacetniData, f, indent=2)
